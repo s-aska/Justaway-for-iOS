c042ca3704369c83354ca5c0252b2a20c220242b
Shinichiro Aska <s.aska.org@gmail.com>
2014-03-20 14:41:25 +0900

    Guy to try UIScrollView

--- Justaway/JFIMainViewController.h
+++ Justaway/JFIMainViewController.h
@@ -1,6 +1,12 @@
 #import <UIKit/UIKit.h>
 
-@interface JFIMainViewController : UIViewController
+@interface JFIMainViewController : UIViewController <UIScrollViewDelegate>
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+
+- (IBAction)timelineAction:(id)sender;
+- (IBAction)notificationAction:(id)sender;
+- (IBAction)directMessageAction:(id)sender;
 
 - (IBAction)accountAction:(id)sender;
 - (IBAction)postAction:(id)sender;
--- Justaway/JFIMainViewController.m
+++ Justaway/JFIMainViewController.m
@@ -23,6 +23,31 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     NSLog(@"[JFIMainViewController] viewDidLoad");
+
+    self.scrollView.pagingEnabled = YES;
+
+    // UIScrollViewに表示するコンテンツViewを作成する。
+    CGSize s = self.scrollView.frame.size;
+    CGRect contentRect = CGRectMake(0, 0, s.width * 3, s.height);
+    UIView *contentView = [[UIView alloc] initWithFrame:contentRect];
+    // コンテンツViewに表示する緑色Viewを追加する。
+    UIView *subContent1View = [[UIView alloc] initWithFrame:CGRectMake(320 * 0, 0, s.width, s.height)];
+    subContent1View.backgroundColor = [UIColor greenColor];
+    [contentView addSubview:subContent1View];
+    // コンテンツViewに表示する青色Viewを追加する。
+    UIView *subContent2View = [[UIView alloc] initWithFrame:CGRectMake(320 * 1, 0, s.width, s.height)];
+    subContent2View.backgroundColor = [UIColor blueColor];
+    [contentView addSubview:subContent2View];
+    // コンテンツViewに表示する赤色Viewを追加する。
+    UIView *subContent3View = [[UIView alloc] initWithFrame:CGRectMake(320 * 2, 0, s.width, s.height)];
+    subContent3View.backgroundColor = [UIColor redColor];
+    [contentView addSubview:subContent3View];
+    // スクロールViewにコンテンツViewを追加する。
+    [self.scrollView addSubview:contentView];
+
+    self.scrollView.contentSize = contentView.frame.size;
+
+    self.scrollView.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,7 +56,57 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    // 現在の表示位置（左上）のx座標とUIScrollViewの表示幅(320px)を
+    // 用いて現在のページ番号を計算します。
+    CGPoint offset = scrollView.contentOffset;
+    int page = (offset.x + 160) / 320;
+    
+    NSLog(@"[JFIMainViewController] scrollViewDidScroll page:%i", page);
+
+    // 現在表示しているページ番号と異なる場合には、
+    // ページ切り替わりと判断し、処理を呼び出します。
+    // currentPageは、現在ページ数を保持するフィールド変数。
+//    if (currentPage != page) {
+//        doSomethingWhenPagingOccurred();
+//        currentPage = page;
+//    }
+}
+
 #pragma mark - Action
+
+- (IBAction)timelineAction:(id)sender
+{
+    [UIView animateWithDuration:.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         [self.scrollView setContentOffset:CGPointMake(0, 0) animated:NO];
+                     } completion:nil];
+}
+
+- (IBAction)notificationAction:(id)sender
+{
+    [UIView animateWithDuration:.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         [self.scrollView setContentOffset:CGPointMake(320, 0) animated:NO];
+                     } completion:nil];
+}
+
+- (IBAction)directMessageAction:(id)sender
+{
+    [UIView animateWithDuration:.3
+                          delay:0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         [self.scrollView setContentOffset:CGPointMake(320 * 2, 0) animated:NO];
+                     } completion:nil];
+}
 
 - (IBAction)postAction:(id)sender
 {
