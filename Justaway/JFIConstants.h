0f05cd7076828ea2bfa07c5f4b7e12d340c76dfc
Shinichiro Aska <s.aska.org@gmail.com>
2014-03-27 21:35:24 +0900

    定食

--- Justaway/JFIConstants.h
+++ Justaway/JFIConstants.h
@@ -1,12 +1,12 @@
 
 #pragma mark - JFIAccountKeys
 
-extern NSString const* JFIAccountOAuthTokenKey;
-extern NSString const* JFIAccountOAuthTokenSecretKey;
-extern NSString const* JFIAccountUserIDKey;
-extern NSString const* JFIAccountScreenNameKey;
-extern NSString const* JFIAccountDisplayNameKey;
-extern NSString const* JFIAccountProfileImageURLKey;
+extern NSString *const JFIAccountOAuthTokenKey;
+extern NSString *const JFIAccountOAuthTokenSecretKey;
+extern NSString *const JFIAccountUserIDKey;
+extern NSString *const JFIAccountScreenNameKey;
+extern NSString *const JFIAccountDisplayNameKey;
+extern NSString *const JFIAccountProfileImageURLKey;
 
 #pragma mark - keychain const
 
--- Justaway/JFIConstants.m
+++ Justaway/JFIConstants.m
@@ -1,12 +1,12 @@
 
 #pragma mark - JFIAccountKeys
 
-NSString const* JFIAccountOAuthTokenKey       = @"oauthToken";
-NSString const* JFIAccountOAuthTokenSecretKey = @"oauthTokenSecret";
-NSString const* JFIAccountUserIDKey           = @"userID";
-NSString const* JFIAccountScreenNameKey       = @"screenName";
-NSString const* JFIAccountDisplayNameKey      = @"displayName";
-NSString const* JFIAccountProfileImageURLKey  = @"profileImageUrl";
+NSString *const JFIAccountOAuthTokenKey       = @"oauthToken";
+NSString *const JFIAccountOAuthTokenSecretKey = @"oauthTokenSecret";
+NSString *const JFIAccountUserIDKey           = @"userID";
+NSString *const JFIAccountScreenNameKey       = @"screenName";
+NSString *const JFIAccountDisplayNameKey      = @"displayName";
+NSString *const JFIAccountProfileImageURLKey  = @"profileImageUrl";
 
 #pragma mark - keychain const
 
