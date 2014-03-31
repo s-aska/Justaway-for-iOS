121c90701f0f5bfcb89be3c55991c4a0113c6ee1
Shinichiro Aska <s.aska.org@gmail.com>
2014-04-01 00:05:44 +0900

    Remove copyright.

--- Justaway/JFIMessagesViewController.h
+++ Justaway/JFIMessagesViewController.h
@@ -1,11 +1,3 @@
-//
-//  JFIDirectMessagesViewController.h
-//  Justaway
-//
-//  Created by Shinichiro Aska on 2014/03/31.
-//  Copyright (c) 2014年 Shinichiro Aska. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface JFIMessagesViewController : UIViewController
--- Justaway/JFINotificationsViewController.m
+++ Justaway/JFINotificationsViewController.m
@@ -1,11 +1,3 @@
-//
-//  JFINotificationsViewController.m
-//  Justaway
-//
-//  Created by Shinichiro Aska on 2014/03/31.
-//  Copyright (c) 2014年 Shinichiro Aska. All rights reserved.
-//
-
 #import "JFINotificationsViewController.h"
 
 @interface JFINotificationsViewController ()
