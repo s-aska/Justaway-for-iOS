499c3a5581142a7d937707ce29f8f93089d0e79b
Shinichiro Aska <s.aska.org@gmail.com>
2014-04-14 22:16:55 +0900

    Set button color.

--- Justaway/JFIButton.m
+++ Justaway/JFIButton.m
@@ -6,6 +6,9 @@
 {
     [super awakeFromNib];
     [self.titleLabel setFont:[UIFont fontWithName:@"fontello" size:self.titleLabel.font.pointSize]];
+    [self setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+    [self setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+    [self setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
 }
 
 - (id)initWithFrame:(CGRect)frame
@@ -13,6 +16,9 @@
     self = [super initWithFrame:frame];
     if (self) {
         [self.titleLabel setFont:[UIFont fontWithName:@"fontello" size:self.titleLabel.font.pointSize]];
+        [self setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [self setTitleColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
+        [self setTitleColor:[UIColor lightGrayColor] forState:UIControlStateDisabled];
     }
     return self;
 }
