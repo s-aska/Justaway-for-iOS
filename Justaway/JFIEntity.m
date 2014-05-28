4d31c6aac306b8144454d6193f90fae9df652226
Shinichiro Aska <s.aska.org@gmail.com>
2014-05-28 22:55:29 +0900

    Decode html.

--- Justaway/JFIEntity.m
+++ Justaway/JFIEntity.m
@@ -45,7 +45,7 @@
         self.screenName = [source valueForKeyPath:@"user.screen_name"];
         self.displayName = [source valueForKeyPath:@"user.name"];
         self.profileImageURL = [[NSURL alloc] initWithString:[source valueForKeyPath:@"user.profile_image_url"]];
-        self.text = [source valueForKey:@"text"];
+        self.text = [self getText:[source valueForKey:@"text"]];
         self.createdAt = [source valueForKey:@"created_at"];
         self.clientName = [self getClientName:[source valueForKey:@"source"]];
         self.retweetCount = [source valueForKey:@"retweet_count"];
@@ -68,7 +68,7 @@
         self.screenName = [message valueForKeyPath:@"sender.screen_name"];
         self.displayName = [message valueForKeyPath:@"sender.name"];
         self.profileImageURL = [[NSURL alloc] initWithString:[message valueForKeyPath:@"sender.profile_image_url"]];
-        self.text = [message valueForKey:@"text"];
+        self.text = [self getText:[message valueForKey:@"text"]];
         self.createdAt = [message valueForKey:@"created_at"];
         self.urls = [message valueForKeyPath:@"entities.urls"];
         self.userMentions = [message valueForKeyPath:@"entities.user_mentions"];
@@ -76,6 +76,13 @@
         self.media = [message valueForKeyPath:@"entities.media"];
     }
     return self;
+}
+
+- (NSString *)getText:(NSString *)text
+{
+    text = [text stringByReplacingOccurrencesOfString:@"&lt;" withString:@"<"];
+    text = [text stringByReplacingOccurrencesOfString:@"&gt;" withString:@">"];
+    return text;
 }
 
 - (NSString *)getClientName:(NSString *)source
