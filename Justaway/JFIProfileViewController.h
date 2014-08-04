b7f2addaf1d8df4dd9eb7baa610aeeddeaa4b76f
Shinichiro Aska <s.aska.org@gmail.com>
2014-08-04 23:38:25 +0900

    Add profile view.

---
+++ Justaway/JFIProfileViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface JFIProfileViewController : UIViewController
+
+@end
---
+++ Justaway/JFIProfileViewController.m
@@ -0,0 +1,27 @@
+#import "JFIProfileViewController.h"
+
+@interface JFIProfileViewController ()
+
+@end
+
+@implementation JFIProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
