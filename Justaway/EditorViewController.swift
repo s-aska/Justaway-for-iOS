14dbad690824db2e45382a7794a6bccfe0d8b6ea
Shinichiro Aska <s.aska.org@gmail.com>
2014-09-16 21:05:16 +0900

    Refactor unused code.

--- Justaway/EditorViewController.swift
+++ Justaway/EditorViewController.swift
@@ -10,14 +10,6 @@ class EditorViewController: UIViewController, UITextViewDelegate {
     var editorTextViewMinHeight: NSNumber!
     let editorTextViewMargin: NSNumber = 20
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
