1a938a98e2381f62ed941ef07e17c44e97ad1ebb
Shinichiro Aska <s.aska.org@gmail.com>
2014-09-26 04:55:33 +0900

    Add loading AcAccount.

--- Justaway/AccountViewController.swift
+++ Justaway/AccountViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Accounts
 
 class AccountViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -14,7 +15,7 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     @IBOutlet weak var leftButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
     
-    var items:Array<String> = ["one", "two", "three", "four"]
+    var rows:Array<Account> = []
     
     override var nibName: String {
         return "AccountViewController"
@@ -32,14 +33,14 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     // MARK: - UITableViewDataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return rows.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
         cell.accessoryType = UITableViewCellAccessoryType.Checkmark
-        cell.textLabel?.text = "\(self.items[indexPath.row])"
-        cell.detailTextLabel?.text = "Subtitle index : \(indexPath.row)"
+        cell.textLabel?.text = self.rows[indexPath.row].name
+        cell.detailTextLabel?.text = self.rows[indexPath.row].screenName
         return cell
     }
     
@@ -60,9 +61,47 @@ class AccountViewController: UIViewController, UITableViewDataSource, UITableVie
     
     // MARK: - Actions
     
+    func alertWithTitle(title: String, message: String) {
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func left(sender: UIButton) {
         if (tableView.editing == true) {
             cancel()
+        } else {
+            let accountStore = ACAccountStore()
+            let accountType = accountStore.accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+            // Prompt the user for permission to their twitter account stored in the phone's settings
+            accountStore.requestAccessToAccountsWithType(accountType, options: nil) {
+                granted, error in
+                
+                if granted {
+                    let twitterAccounts = accountStore.accountsWithAccountType(accountType)
+                    
+                    if twitterAccounts?.count == 0 {
+                        self.alertWithTitle("Error", message: "There are no Twitter accounts configured. You can add or create a Twitter account in Settings.")
+                    } else {
+                        self.rows = twitterAccounts.map({
+                            twitterAccount in
+                            Account(accessToken: twitterAccount.identifier,
+                                userID: twitterAccount.valueForKeyPath("properties.user_id") as String,
+                                screenName: twitterAccount.username as String,
+                                name: twitterAccount.username as String,
+                                profileImageURL: NSURL(string: ""),
+                                iOS: true)
+                        })
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.tableView.reloadData()
+                        })
+                    }
+                } else {
+                    self.alertWithTitle("Error", message: error.localizedDescription)
+                }
+            }
+            
         }
     }
     
