0de13523468669cc153c2ad1bc291ed59cb0542c
Shinichiro Aska <s.aska.org@gmail.com>
2014-09-29 21:18:23 +0900

    Refactor code.

--- Justaway/AccountService.swift
+++ Justaway/AccountService.swift
@@ -2,44 +2,41 @@ import Foundation
 
 class AccountService {
     
-    struct KeyConstants {
-        static let keychain = "AccountService"
-        static let current = "current"
-        static let accounts = "accounts"
+    struct Constants {
+        static let Keychain = "AccountService"
+        static let Current = "current"
+        static let Accounts = "accounts"
     }
     
     class func save(current: NSNumber, accounts: Array<Account>) -> Bool {
         let dictionary = [
-            KeyConstants.current  : current,
-            KeyConstants.accounts : accounts.map({ (account: Account) -> NSDictionary in account.toDictionary() })
+            Constants.Current  : current,
+            Constants.Accounts : accounts.map({ (a) in a.toDictionary() })
         ]
         
         let data = NSJSONSerialization.dataWithJSONObject(dictionary, options: nil, error: nil)!
         
-        return KeychainService.save(KeyConstants.keychain, data: data)
+        return KeychainService.save(Constants.Keychain, data: data)
     }
     
     class func load() -> (NSNumber, Array<Account>) {
-        let data = KeychainService.load(KeyConstants.keychain)
+        let data = KeychainService.load(Constants.Keychain)
         
         if data == nil {
             return (-1, Array<Account>())
         }
         
-        let json :AnyObject! = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.AllowFragments, error: nil) as? NSDictionary
+        let json :AnyObject! = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil)
         
-        let current = json[KeyConstants.current] as NSNumber
+        let current = json[Constants.Current] as NSNumber
         
-        var accounts :Array<Account> = (json[KeyConstants.accounts] as [NSDictionary]).map({
-            (dictionary: NSDictionary) in
-            Account(dictionary: dictionary)
-        })
+        var accounts :Array<Account> = (json[Constants.Accounts] as [NSDictionary]).map({ (d) in Account(dictionary: d) })
         
         return (current, accounts)
     }
     
     class func clear() {
-        KeychainService.remove(KeyConstants.keychain)
+        KeychainService.remove(Constants.Keychain)
     }
     
 }
