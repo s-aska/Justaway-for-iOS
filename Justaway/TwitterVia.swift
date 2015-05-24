8d7c427dfdd312819aa63422d5a3b0fcdf2a2441
Shinichiro Aska <s.aska.org@gmail.com>
2015-05-24 16:02:02 +0900

    firstMatchInString's range need count(source.utf16)

--- Justaway/TwitterVia.swift
+++ Justaway/TwitterVia.swift
@@ -13,7 +13,7 @@ struct TwitterVia {
     
     init(_ source: String) {
         let s = source as NSString
-        if let match = Static.regexp.firstMatchInString(source, options: NSMatchingOptions(0), range: NSMakeRange(0, count(source))) {
+        if let match = Static.regexp.firstMatchInString(source, options: NSMatchingOptions(0), range: NSMakeRange(0, count(source.utf16))) {
             if match.numberOfRanges > 0 {
                 self.URL = NSURL(string: s.substringWithRange(match.rangeAtIndex(1)) as String)
                 self.name = s.substringWithRange(match.rangeAtIndex(2))
