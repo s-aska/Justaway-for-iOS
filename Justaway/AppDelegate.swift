a4823472d2266627e9cec7404867a31b40823f52
Shinichiro Aska <s.aska.org@gmail.com>
2015-10-01 05:03:14 +0900

    OAuth signing with SafariOAuthURLHandler - typo

--- Justaway/AppDelegate.swift
+++ Justaway/AppDelegate.swift
@@ -76,7 +76,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
         if url.absoluteString.hasPrefix("justaway://success") ?? false {
-            SafariOAuthSwiftURLHandler.callback(url)
+            SafariOAuthURLHandler.callback(url)
         }
         
         return true
