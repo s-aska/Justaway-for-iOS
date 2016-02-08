e3ac2c8771e5cf69927cd45ff3cb31e4a1020a4e
Shinichiro Aska <s.aska.org@gmail.com>
2016-02-08 18:22:38 +0900

    Fix build error swiftlint autocorrect

--- Justaway/TabSettingsCell.swift
+++ Justaway/TabSettingsCell.swift
@@ -9,21 +9,21 @@
 import UIKit
 
 class TabSettingsCell: BackgroundTableViewCell {
-    
+
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var displayNameLabel: DisplayNameLable!
     @IBOutlet weak var screenNameLabel: ScreenNameLable!
     @IBOutlet weak var clientNameLabel: ClientNameLable!
-    
+
     // MARK: - View Life Cycle
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         configureView()
     }
-    
+
     // MARK: - Configuration
-    
+
     func configureView() {
         selectionStyle = .None
         separatorInset = UIEdgeInsetsZero
