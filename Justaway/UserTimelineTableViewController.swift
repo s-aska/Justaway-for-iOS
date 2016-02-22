326f1dbd34cfd6670a695384ae7950124928810d
Shinichiro Aska <s.aska.org@gmail.com>
2016-02-22 20:35:23 +0900

    Add cache for user

--- Justaway/UserTimelineTableViewController.swift
+++ Justaway/UserTimelineTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import KeyClip
+import Async
 
 class UserTimelineTableViewController: StatusTableViewController {
 
@@ -15,11 +16,40 @@ class UserTimelineTableViewController: StatusTableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        cacheLoaded = true // no cache
         adapter.scrollEnd(tableView) // contentInset call scrollViewDidScroll, but call scrollEnd
     }
 
     override func saveCache() {
+        if self.adapter.rows.count > 0 {
+            if let userID = self.userID {
+                let key = "user:\(userID)"
+                let statuses = self.adapter.statuses
+                let dictionary = ["statuses": ( statuses.count > 100 ? Array(statuses[0 ..< 100]) : statuses ).map({ $0.dictionaryValue })]
+                KeyClip.save(key, dictionary: dictionary)
+                NSLog("\(key) saveCache.")
+            }
+        }
+    }
+
+    override func loadCache(success: ((statuses: [TwitterStatus]) -> Void), failure: ((error: NSError) -> Void)) {
+        if let userID = self.userID {
+            let key = "user:\(userID)"
+            Async.background {
+                if let cache = KeyClip.load(key) as NSDictionary? {
+                    if let statuses = cache["statuses"] as? [[String: AnyObject]] {
+                        success(statuses: statuses.map({ TwitterStatus($0) }))
+                        return
+                    }
+                }
+                success(statuses: [TwitterStatus]())
+
+                Async.background(after: 0.5, block: { () -> Void in
+                    self.loadData(nil)
+                })
+            }
+        } else {
+            success(statuses: [])
+        }
     }
 
     override func loadData(maxID: String?, success: ((statuses: [TwitterStatus]) -> Void), failure: ((error: NSError) -> Void)) {
